cc8fa047b5cc8428a0e89c0519f293f7a8375405
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-12-10 01:18:11 +0200

    feat: created master and detail controller

---
+++ SplitViews/DetailController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailController.swift
+//  SplitViews
+//
+//  Created by Sergey Lukaschuk on 10.12.2021.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Detail"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
---
+++ SplitViews/MasterController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterController.swift
+//  SplitViews
+//
+//  Created by Sergey Lukaschuk on 10.12.2021.
+//
+
+import UIKit
+
+class MasterController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Master"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
