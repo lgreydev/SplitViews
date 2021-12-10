200696f4b1a23f2d8e5b95669deda74421066c68
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-12-11 00:10:30 +0200

    feat: add table view

--- SplitViews/MasterController.swift
+++ SplitViews/MasterController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class MasterController: UITableViewController {
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        self.tableView = UITableView(frame: .zero, style: .insetGrouped)
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "id")
+        
         self.navigationItem.title = "Master"
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+   
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "id")!
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+    }
 }
